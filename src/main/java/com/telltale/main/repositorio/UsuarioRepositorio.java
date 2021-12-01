ccae19b4e4cbebbe205e10b1c7c0a358706dcd81
catecunsolo <caterinacunsolo0@gmail.com>
2021-12-01 20:43:24 -0300

    Usuario entidad y repositorio

--- src/main/java/com/telltale/main/repositorio/UsuarioRepositorio.java
+++ src/main/java/com/telltale/main/repositorio/UsuarioRepositorio.java
@@ -1,4 +1,9 @@
 package com.telltale.main.repositorio;
 
-public class UsuarioRepositorio {
+import com.telltale.main.entidad.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepositorio extends JpaRepository<Usuario,Integer> {
 }
--- src/main/java/com/telltale/main/servicio/UsuarioServicio.java
+++ src/main/java/com/telltale/main/servicio/UsuarioServicio.java
@@ -1,4 +1,12 @@
 package com.telltale.main.servicio;
 
+import com.telltale.main.repositorio.UsuarioRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UsuarioServicio {
+
+    @Autowired
+    private UsuarioRepositorio usuarioRepositorio;
 }
