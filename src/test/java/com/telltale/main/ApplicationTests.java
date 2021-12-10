dc5b78ab7333a4fd8d08a79dfe7fd1d73970624e
Marcos G. Frites <marcos.detti@hotmail.com>
2021-12-09 21:24:17 -0300

    Creación de método para relleno de historias en la DB

--- src/test/java/com/telltale/main/ApplicationTests.java
+++ src/test/java/com/telltale/main/ApplicationTests.java
@@ -1,9 +1,6 @@
 package com.telltale.main;
 
-import com.telltale.main.entidad.Categoria;
-import com.telltale.main.entidad.Perfil;
-import com.telltale.main.entidad.Rol;
-import com.telltale.main.entidad.Usuario;
+import com.telltale.main.entidad.*;
 import com.telltale.main.repositorio.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -238,4 +235,95 @@ class ApplicationTests {
 		categoriaRepositorio.save(categoria7);
 	}
 
+	@Test
+	public void testHistoriaRepositorio_deleteAll() { //método para borrar, desde HistoriaRepositorio, todos los datos cargados en la tabla HISTORIA de la DB
+		historiaRepositorio.deleteAll();
+	}
+
+	@Test
+	public void testHistoriaRepositorio_save() { //método para cargar datos, desde HistoriaRepositorio, a la tabla HISTORIA de la DB
+		Historia historia1 = new Historia();
+		historia1.setTitulo("Titulo de Historia 1");
+		historia1.setHistoria("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's.");
+		historia1.setMeGusta(7);
+		historia1.setNoMeGusta(1);
+		historia1.setCategoria(categoriaRepositorio.findById(1).get());
+		historia1.setPerfil(perfilRepositorio.findById(1).get());
+		historia1.setFechaCreacion(LocalDate.now());
+		historia1.setFechaUltModificacion(LocalDate.now());
+		historia1.setAlta(true);
+		historiaRepositorio.save(historia1);
+
+		Historia historia2 = new Historia();
+		historia2.setTitulo("Titulo de Historia 2");
+		historia2.setHistoria("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's.");
+		historia2.setMeGusta(6);
+		historia2.setNoMeGusta(2);
+		historia2.setCategoria(categoriaRepositorio.findById(2).get());
+		historia2.setPerfil(perfilRepositorio.findById(2).get());
+		historia2.setFechaCreacion(LocalDate.now());
+		historia2.setFechaUltModificacion(LocalDate.now());
+		historia2.setAlta(true);
+		historiaRepositorio.save(historia2);
+
+		Historia historia3 = new Historia();
+		historia3.setTitulo("Titulo de Historia 3");
+		historia3.setHistoria("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's.");
+		historia3.setMeGusta(5);
+		historia3.setNoMeGusta(3);
+		historia3.setCategoria(categoriaRepositorio.findById(3).get());
+		historia3.setPerfil(perfilRepositorio.findById(3).get());
+		historia3.setFechaCreacion(LocalDate.now());
+		historia3.setFechaUltModificacion(LocalDate.now());
+		historia3.setAlta(true);
+		historiaRepositorio.save(historia3);
+
+		Historia historia4 = new Historia();
+		historia4.setTitulo("Titulo de Historia 4");
+		historia4.setHistoria("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's.");
+		historia4.setMeGusta(4);
+		historia4.setNoMeGusta(4);
+		historia4.setCategoria(categoriaRepositorio.findById(4).get());
+		historia4.setPerfil(perfilRepositorio.findById(4).get());
+		historia4.setFechaCreacion(LocalDate.now());
+		historia4.setFechaUltModificacion(LocalDate.now());
+		historia4.setAlta(true);
+		historiaRepositorio.save(historia4);
+
+		Historia historia5 = new Historia();
+		historia5.setTitulo("Titulo de Historia 5");
+		historia5.setHistoria("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's.");
+		historia5.setMeGusta(3);
+		historia5.setNoMeGusta(5);
+		historia5.setCategoria(categoriaRepositorio.findById(5).get());
+		historia5.setPerfil(perfilRepositorio.findById(5).get());
+		historia5.setFechaCreacion(LocalDate.now());
+		historia5.setFechaUltModificacion(LocalDate.now());
+		historia5.setAlta(true);
+		historiaRepositorio.save(historia5);
+
+		Historia historia6 = new Historia();
+		historia6.setTitulo("Titulo de Historia 6");
+		historia6.setHistoria("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's.");
+		historia6.setMeGusta(2);
+		historia6.setNoMeGusta(6);
+		historia6.setCategoria(categoriaRepositorio.findById(6).get());
+		historia6.setPerfil(perfilRepositorio.findById(6).get());
+		historia6.setFechaCreacion(LocalDate.now());
+		historia6.setFechaUltModificacion(LocalDate.now());
+		historia6.setAlta(true);
+		historiaRepositorio.save(historia6);
+
+		Historia historia7 = new Historia();
+		historia7.setTitulo("Titulo de Historia 7");
+		historia7.setHistoria("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's.");
+		historia7.setMeGusta(1);
+		historia7.setNoMeGusta(7);
+		historia7.setCategoria(categoriaRepositorio.findById(7).get());
+		historia7.setPerfil(perfilRepositorio.findById(7).get());
+		historia7.setFechaCreacion(LocalDate.now());
+		historia7.setFechaUltModificacion(LocalDate.now());
+		historia7.setAlta(true);
+		historiaRepositorio.save(historia7);
+	}
 }
