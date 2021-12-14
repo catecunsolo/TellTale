a5492ab9b69ca347df45346b0367e21af5852374
Marcos G. Frites <marcos.detti@hotmail.com>
2021-12-14 20:20:54 -0300

    Integracion y actualizacion del FRONTEND

--- src/test/java/com/telltale/main/ApplicationTests.java
+++ src/test/java/com/telltale/main/ApplicationTests.java
@@ -77,7 +77,6 @@ class ApplicationTests {
 	public void testUsuarioRepositorio_deleteAll() { //método para borrar, desde UsuarioRepositorio, todos los datos cargados en la tabla USUARIO de la DB
 		usuarioRepositorio.deleteAll();
 	}
-*/
 
 	@Test
 	public void testUsuarioRepositorio_save() { //método para cargar datos, desde UsuarioRepositorio, a la tabla USUARIO de la DB
@@ -152,7 +151,6 @@ class ApplicationTests {
 		usuarioRepositorio.save(usuario7);
 	}
 
-	/*
         @Test
         public void testPerfilRepositorio_deleteAll() { //método para borrar, desde PerfilRepositorio, todos los datos cargados en la tabla PERFIL de la DB
             perfilRepositorio.deleteAll();
@@ -224,11 +222,12 @@ class ApplicationTests {
             perfilRepositorio.save(perfil7);
         }
 
-    */
 	@Test
 	public void testCategoriaRepositorio_deleteAll() { //método para borrar, desde CategoriaRepositorio, todos los datos cargados en la tabla CATEGORIA de la DB
 		categoriaRepositorio.deleteAll();
 	}
+    */
+
 
 	@Test
 	public void testCategoriaRepositorio_save() { //método para cargar datos, desde CategoriaRepositorio, a la tabla CATEGORIA de la DB
