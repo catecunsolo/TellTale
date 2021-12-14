436d137b90cc5e7545eb89abaefd3bd4663718f5
Marcos G. Frites <marcos.detti@hotmail.com>
2021-12-14 20:41:08 -0300

    Correccion ApplicationTests y Categoria.java

--- src/test/java/com/telltale/main/ApplicationTests.java
+++ src/test/java/com/telltale/main/ApplicationTests.java
@@ -233,81 +233,51 @@ class ApplicationTests {
 	public void testCategoriaRepositorio_save() { //método para cargar datos, desde CategoriaRepositorio, a la tabla CATEGORIA de la DB
 		Categoria categoria = new Categoria();
 		categoria.setNombre("PANQUEQUE");
-		categoria.setFechaCreacion(LocalDate.now());
-		categoria.setFechaUltModificacion(LocalDate.now());
-		categoria.setAlta(true);
 		categoria.setVoto((int) ((Math.random())*100)+1);
 		categoriaRepositorio.save(categoria);
 
 		categoria = new Categoria();
 		categoria.setNombre("MEDIALUNAS");
-		categoria.setFechaCreacion(LocalDate.now());
-		categoria.setFechaUltModificacion(LocalDate.now());
-		categoria.setAlta(true);
 		categoria.setVoto((int) ((Math.random())*100)+1);
 		categoriaRepositorio.save(categoria);
 
 		categoria = new Categoria();
 		categoria.setNombre("GÁRGOLA");
-		categoria.setFechaCreacion(LocalDate.now());
-		categoria.setFechaUltModificacion(LocalDate.now());
-		categoria.setAlta(true);
 		categoria.setVoto((int) ((Math.random())*100)+1);
 		categoriaRepositorio.save(categoria);
 
 		categoria = new Categoria();
 		categoria.setNombre("SAUCE");
-		categoria.setFechaCreacion(LocalDate.now());
-		categoria.setFechaUltModificacion(LocalDate.now());
-		categoria.setAlta(true);
 		categoria.setVoto((int) ((Math.random())*100)+1);
 		categoriaRepositorio.save(categoria);
 
 		categoria = new Categoria();
 		categoria.setNombre("COLIBRÍ");
-		categoria.setFechaCreacion(LocalDate.now());
-		categoria.setFechaUltModificacion(LocalDate.now());
-		categoria.setAlta(true);
 		categoria.setVoto((int) ((Math.random())*100)+1);
 		categoriaRepositorio.save(categoria);
 
 		categoria = new Categoria();
 		categoria.setNombre("MONTAÑA");
-		categoria.setFechaCreacion(LocalDate.now());
-		categoria.setFechaUltModificacion(LocalDate.now());
-		categoria.setAlta(true);
 		categoria.setVoto((int) ((Math.random())*100)+1);
 		categoriaRepositorio.save(categoria);
 
 		categoria = new Categoria();
 		categoria.setNombre("MAR");
-		categoria.setFechaCreacion(LocalDate.now());
-		categoria.setFechaUltModificacion(LocalDate.now());
-		categoria.setAlta(true);
 		categoria.setVoto((int) ((Math.random())*100)+1);
 		categoriaRepositorio.save(categoria);
 
 		categoria = new Categoria();
 		categoria.setNombre("CRUCERO");
-		categoria.setFechaCreacion(LocalDate.now());
-		categoria.setFechaUltModificacion(LocalDate.now());
-		categoria.setAlta(true);
 		categoria.setVoto((int) ((Math.random())*100)+1);
 		categoriaRepositorio.save(categoria);
 
 		categoria = new Categoria();
 		categoria.setNombre("TRABAJO");
-		categoria.setFechaCreacion(LocalDate.now());
-		categoria.setFechaUltModificacion(LocalDate.now());
-		categoria.setAlta(true);
 		categoria.setVoto((int) ((Math.random())*100)+1);
 		categoriaRepositorio.save(categoria);
 
 		categoria = new Categoria();
 		categoria.setNombre("AMOR");
-		categoria.setFechaCreacion(LocalDate.now());
-		categoria.setFechaUltModificacion(LocalDate.now());
-		categoria.setAlta(true);
 		categoria.setVoto((int) ((Math.random())*100)+1);
 		categoriaRepositorio.save(categoria);
 
