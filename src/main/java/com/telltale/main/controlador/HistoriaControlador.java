514000fab37e632255d00223a017d25d9461ab51
Seba Gimenez <seba_gimenez97@hotmail.com.ar>
2021-12-15 12:46:59 -0300

    Cambios de mi controller

--- src/main/java/com/telltale/main/controlador/HistoriaControlador.java
+++ src/main/java/com/telltale/main/controlador/HistoriaControlador.java
@@ -6,11 +6,14 @@
 package com.telltale.main.controlador;
 
 import com.telltale.main.entidad.Categoria;
+import com.telltale.main.entidad.Historia;
 import com.telltale.main.entidad.Perfil;
 import com.telltale.main.servicio.CategoriaServicio;
 import com.telltale.main.servicio.HistoriaServicio;
 import com.telltale.main.servicio.PerfilServicio;
 import com.telltale.main.servicio.UsuarioServicio;
+import java.util.Collections;
+import java.util.List;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -30,7 +33,7 @@ import org.springframework.web.servlet.view.RedirectView;
 @Controller
 @RequestMapping("/historia")
 public class HistoriaControlador {
-
+    
     @Autowired
     private HistoriaServicio historiaServicio;
     @Autowired
@@ -42,57 +45,81 @@ public class HistoriaControlador {
     private UsuarioServicio usuarioServicio;
 
     //implementa las categorias del dia
-    @GetMapping("/crear")
+    @PostMapping("/crear")
     public ModelAndView crear(HttpSession session) {
-        ModelAndView mv = new ModelAndView("crearHistoriaPrueba");
-        //Acá hay que implementar el método que te devuelve las categorías del día
-        //mv.addObject("categorias", categoriaServicio.categoriasDelDia);
+        ModelAndView mv = new ModelAndView("historias");
+        mv.addObject("action", "crear");
+        Perfil perfil = perfilServicio.buscarPerfilPorId(Integer.parseInt(session.getId()));
+        mv.addObject("categoria",perfil.getCategoriaDelDia());
         return mv;
     }
-
-    //Implementar Session
+    
     @PostMapping("/guardar")
     public RedirectView guardar(@RequestParam("titulo") String titulo, @RequestParam("historia") String historia,
-            @RequestParam("id_categoria") int id_categoria, RedirectAttributes attributes, HttpSession session) {
+        RedirectAttributes attributes, HttpSession session) {
         RedirectView rv = new RedirectView("/historia");
         
         try {
-            historiaServicio.crearHistoria(titulo, historia,
-                    perfilServicio.buscarPerfilPorIdUsuario(Integer.parseInt(session.getId())),
-                    categoriaServicio.buscarCategoriaPorId(id_categoria));
+            Perfil perfil= perfilServicio.buscarPerfilPorIdUsuario(Integer.parseInt(session.getId()));
+            historiaServicio.crearHistoria(titulo, historia,perfil,perfil.getCategoriaDelDia());
             attributes.addFlashAttribute("exito-name", "Historia guardada");
         } catch (Exception e) {
             attributes.addFlashAttribute("error-name", e.getMessage());
             rv.setUrl("/historia/crear");
         }
-
+        
         return rv;
     }
-
     
     @GetMapping()
     public ModelAndView historia(HttpSession session) {
-        ModelAndView mv = new ModelAndView("");
+        ModelAndView mv = new ModelAndView("historia");
+        mv.addObject("action", "historias");
         try {
             Perfil perfil = perfilServicio.buscarPerfilPorIdUsuario(Integer.parseInt(session.getId()));
             Categoria categoria = perfil.getCategoriaDelDia();
-            if (categoria== null) {
+            if (categoria == null) {
+                //aca tengo que poner la ruta del controlador de categorias
+                mv.setViewName("redirect:/");
 //                mv.addObject("Historia1",
 //                        historiaServicio.verHistoriasPorCategoria(categoriaServicio.categoriasDelDia.get(0)).get(0));
 //                mv.addObject("Historia2",
 //                        historiaServicio.verHistoriasPorCategoria(categoriaServicio.categoriasDelDia.get(1)).get(0));
 //                mv.addObject("Historia3",
 //                        historiaServicio.verHistoriasPorCategoria(categoriaServicio.categoriasDelDia.get(2)).get(0));
-            }else{
-                mv.addObject("historias", historiaServicio.verHistoriasPorCategoria(categoria));
-                mv.addObject("categoria",categoria);
+            } else {
+                List<Historia> historias = historiaServicio.verHistoriasPorCategoria(categoria);
+                List<Historia> historiasCortadas = Collections.EMPTY_LIST;
+                for (Historia historia : historias) {
+                    String history = historia.getHistoria();
+                    if (history.length() > 90) {
+                        history = history.substring(0, 89);
+                        historia.setHistoria(history);
+                    }
+                    historiasCortadas.add(historia);
+                }
+                mv.addObject("historias", historiasCortadas);
+                mv.addObject("categoria", categoria);
             }
         } catch (Exception e) {
             mv.addObject("error-name", e.getMessage());
         }
         
         return mv;
-
+        
     }
-
+    
+    @GetMapping("/{id}")
+    public ModelAndView historiaEnParticular(@PathVariable int id,RedirectAttributes ra){
+        ModelAndView mv = new ModelAndView("historia");
+        mv.addObject("action",id);
+        try{
+            mv.addObject("historia", historiaServicio.buscarHistoriaPorId(id));
+        }catch(Exception e){
+            ra.addFlashAttribute("error-name", e.getMessage());
+            mv.setViewName("redirect:/historia");
+        }
+        return mv;
+        
+    }
 }
